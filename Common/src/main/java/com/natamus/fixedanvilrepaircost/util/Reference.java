0b4cacf11542edc449d2e44f7c27b715d84b5bed
Rick South <rick@natamus.com>
2024-10-24 19:59:43 +0200

    Fixed Anvil Repair Cost source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/fixedanvilrepaircost/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.fixedanvilrepaircost.util;
+
+public class Reference {
+	public static final String MOD_ID = "fixedanvilrepaircost";
+	public static final String NAME = "Fixed Anvil Repair Cost";
+	public static final String VERSION = "3.4";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
